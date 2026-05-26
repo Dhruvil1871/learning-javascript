4d9e75a818a326adf49894adbf7bfd5ef240647c
Dhruvil Mathukiya <dhruvilm.2612@gmail.com>
2026-05-26 12:22:31 +0530

    .

--- foundations/13_palindromes/palindromes.js
+++
@@ -1,8 +0,0 @@
-const palindromes = function (string) {
-    string = string.toLowerCase().replace(/[^a-z0-9]/g, "");
-    
-    return string === string.split("").reverse().join("");
-};
- 
-// Do not edit below this line
-module.exports = palindromes;
--- foundations/15_getTheTitles/getTheTitles.js
+++
@@ -1,8 +0,0 @@
-const getTheTitles = function(books) {
-    const bookNames = books.map(book => book.title);
-
-    return bookNames
-};
-
-// Do not edit below this line
-module.exports = getTheTitles;
